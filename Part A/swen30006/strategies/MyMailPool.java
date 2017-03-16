fdfe4fc375ac42fd9e9402cf75cc910272f72e6e
ffreylaw <ffreylaw@gmail.com>
2017-03-17 00:03:27 +1100

    update

--- Part A/swen30006/strategies/MyMailPool.java
+++ Part A/swen30006/strategies/MyMailPool.java
@@ -1,6 +1,5 @@
 package strategies;
 
-import java.util.Comparator;
 import java.util.LinkedList;
 
 import automail.IMailPool;
@@ -9,11 +8,9 @@ import automail.MailItem;
 public class MyMailPool implements IMailPool {
 	
 	private LinkedList<MailItem> allMailItems;
-	private LinkedList<MailItem> deliveryMailItems;
 	
 	public MyMailPool() {
 		allMailItems = new LinkedList<MailItem>();
-		deliveryMailItems = new LinkedList<MailItem>();
 	}
 
 	@Override
@@ -26,89 +23,12 @@ public class MyMailPool implements IMailPool {
         return allMailItems.isEmpty();
     }
     
-    public MailItem get(){
-    	return deliveryMailItems.peek();
-    }
-    
     public void remove(MailItem item) {
     	allMailItems.remove(item);
-    	deliveryMailItems.remove(item);
     }
     
-    public void readyForDeliver() {
-    	deliveryMailItems.clear();
-    	
-    	Comparator<MailItem> sortByPriority = new MailItemComparator();
-    	allMailItems.sort(sortByPriority);
-    	
-    	LinkedList<MailItem> tempMailItems = new LinkedList<MailItem>();
-    	String currentStatePriority = null;
-    	for (MailItem item: allMailItems) {
-    		if (currentStatePriority != null) {
-    			if (currentStatePriority == item.getPriorityLevel()) {
-    				tempMailItems.add(item);
-    			} else {
-    				break;
-    			}
-    		} else {
-    			currentStatePriority = item.getPriorityLevel();
-    			tempMailItems.add(item);
-    		}
-    	}
-    	
-    	Comparator<MailItem> sortByDistance = (MailItem o1, MailItem o2) -> Math.abs(o1.getDestFloor() - 3) - Math.abs(o2.getDestFloor() - 3);
-    	Comparator<MailItem> sortBySize = (MailItem o1, MailItem o2) -> o1.getSize() - o2.getSize();
-    	tempMailItems.sort(sortByDistance);
-    	boolean isShortDistance = Math.abs(tempMailItems.getFirst().getDestFloor() - 3) < 3;
-    	if (!isShortDistance) {
-    		tempMailItems.sort(sortBySize);
-    		int smallestSize = tempMailItems.getFirst().getSize();
-    		int earliestArrivalTime = tempMailItems.getFirst().getArrivalTime();
-    		MailItem earliestItem = tempMailItems.getFirst();
-    		for (MailItem item: tempMailItems) {
-    			if (item.getArrivalTime() < earliestArrivalTime) {
-    				if (item.getSize() == smallestSize) {
-	    				earliestArrivalTime = item.getArrivalTime();
-	    				earliestItem = item;
-    				} else {
-    					break;
-    				}
-    			}
-    		}
-    		deliveryMailItems.add(earliestItem);
-    	} else {
-    		deliveryMailItems.add(tempMailItems.getFirst());
-    	}
-    	
-    	boolean isLowerDestFloor = deliveryMailItems.getFirst().getDestFloor() <= 3;
-    	int size = deliveryMailItems.getFirst().getSize();
-    	for (MailItem item: allMailItems) {
-    		if (item != deliveryMailItems.getFirst()) {
-    			if (size + item.getSize() <= 4) {
-	    			if (item.getDestFloor() <= 3 && isLowerDestFloor) {
-	    				deliveryMailItems.add(item);
-		    			size += item.getSize();
-	    			}
-	    			if (item.getDestFloor() > 3 && !isLowerDestFloor) {
-	    				if (Math.abs(item.getDestFloor() - 3) < 3 && isShortDistance) {
-	    					deliveryMailItems.add(item);
-		    				size += item.getSize();
-	    				}
-	    				if (Math.abs(item.getDestFloor() - 3) >= 3 && !isShortDistance) {
-	    					deliveryMailItems.add(item);
-		    				size += item.getSize();
-	    				}
-	    			}
-    			}
-    		}
-    	}
-    	
-    	Comparator<MailItem> sortByDestFloor = (MailItem o1, MailItem o2) -> o2.getDestFloor() - o1.getDestFloor();
-    	deliveryMailItems.sort(sortByDestFloor);
-    }
-    
-    public boolean isEmptyList() {
-    	return deliveryMailItems.isEmpty();
+    public LinkedList<MailItem> getAllMailItems() {
+    	return allMailItems;
     }
  
 }
--- Part A/swen30006/strategies/MyMailSorter.java
+++ Part A/swen30006/strategies/MyMailSorter.java
@@ -1,5 +1,10 @@
 package strategies;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import automail.Building;
 import automail.Clock;
 import automail.IMailSorter;
 import automail.MailItem;
@@ -10,27 +15,29 @@ public class MyMailSorter implements IMailSorter {
 	
 	private MyMailPool myMailPool;
 	
+	/** This delivery array contains mail items 
+	 *  that are going to be in the storage tube of the robot
+	 */
+	private LinkedList<MailItem> deliveryMailItems;
+	
 	public MyMailSorter(MyMailPool myMailPool) {
 		this.myMailPool = myMailPool;
+		deliveryMailItems = new LinkedList<MailItem>();
 	}
 
 	@Override
 	public boolean fillStorageTube(StorageTube tube) {
         try {
+        	/** A new delivery attempt */
             if (!myMailPool.isEmptyPool()) {
-            	if (tube.isEmpty()) {
-            		myMailPool.readyForDeliver();
-            	}
-            	if (!myMailPool.isEmptyList()) {
-	            	/** Gets the first item from the ArrayList */
-		            MailItem mailItem = myMailPool.get();
-		            /** Add the item to the tube */
-		            tube.addItem(mailItem);
-		            /** Remove the item from the ArrayList */
-		            myMailPool.remove(mailItem);
-            	} else {
-            		return true;
-            	}
+            	/** Sort the mail pool then generate the delivery array */
+            	readyForDeliver(tube);
+            	/**  Add each item in the delivery array to the tube */
+            	for (MailItem item: deliveryMailItems) {
+	            	tube.addItem(item);
+	            	myMailPool.remove(item);
+	            }
+            	return true;
             }
         }
         /** Refer to TubeFullException.java --
@@ -49,6 +56,131 @@ public class MyMailSorter implements IMailSorter {
         }
         
 		return false;
+	}
+	
+	public void readyForDeliver(StorageTube tube) {
+		/** Get all mail items from the mail pool */
+		LinkedList<MailItem> allMailItems = myMailPool.getAllMailItems();
+		
+		/** Clear delivery array due to new delivery attempt */
+    	deliveryMailItems.clear();
+    	
+    	/** Sort all mail items by priority level - HIGH -> MEDIUM -> LOW */
+    	Comparator<MailItem> sortByPriority = new MailItemComparator();
+    	allMailItems.sort(sortByPriority);
+    	
+    	/** Create a temporary array to store the mail items that currently
+    	 *  are the highest and the same priority in the allMailItems array
+    	 */
+    	LinkedList<MailItem> tempMailItems = new LinkedList<MailItem>();
+    	String currentStatePriority = null;
+    	for (MailItem item: allMailItems) {
+    		if (currentStatePriority != null) {
+    			if (currentStatePriority == item.getPriorityLevel()) {
+    				tempMailItems.add(item);
+    			} else {
+    				break;
+    			}
+    		} else {
+    			currentStatePriority = item.getPriorityLevel();
+    			tempMailItems.add(item);
+    		}
+    	}
+    	
+    	/** Sort the temporary array by distance 
+    	 *  (distance from the mail room floor to destination floor) 
+    	 */
+    	Comparator<MailItem> sortByDistance = (MailItem o1, MailItem o2) -> Math.abs(o1.getDestFloor() - Building.MAILROOM_LOCATION) - Math.abs(o2.getDestFloor() - Building.MAILROOM_LOCATION);
+    	tempMailItems.sort(sortByDistance);
+    	
+    	/** Detect whether the lowest distance mail item in the array is short distance,
+    	 *  which within the range of the longest distance from mail room to destination floor divided by 2.
+    	 */
+    	int shortDistance = Math.abs(Building.FLOORS - Building.MAILROOM_LOCATION) > Math.abs(Building.LOWEST_FLOOR - Building.MAILROOM_LOCATION) ?
+    						Math.abs(Building.FLOORS - Building.MAILROOM_LOCATION)/2 : Math.abs(Building.LOWEST_FLOOR - Building.MAILROOM_LOCATION)/2;
+    	boolean isShortDistance = Math.abs(tempMailItems.getFirst().getDestFloor() - Building.MAILROOM_LOCATION) < shortDistance;
+    	
+    	/** Selecting the most suitable item to be deliver at current state
+    	 */
+    	if (!isShortDistance) {
+    		/** If the mail item is short distance, then sort the temporary array by size
+        	 *  from smallest to largest, then get the item with the earliest arrival time
+        	 *  in the array, and then add that item to the delivery array
+        	 */
+    		Comparator<MailItem> sortBySize = (MailItem o1, MailItem o2) -> o1.getSize() - o2.getSize();
+    		tempMailItems.sort(sortBySize);
+    		int smallestSize = tempMailItems.getFirst().getSize();
+    		int earliestArrivalTime = tempMailItems.getFirst().getArrivalTime();
+    		MailItem earliestItem = tempMailItems.getFirst();
+    		for (MailItem item: tempMailItems) {
+    			if (item.getArrivalTime() < earliestArrivalTime) {
+    				if (item.getSize() == smallestSize) {
+	    				earliestArrivalTime = item.getArrivalTime();
+	    				earliestItem = item;
+    				} else {
+    					break;
+    				}
+    			}
+    		}
+    		deliveryMailItems.add(earliestItem);
+    	} else {
+    		/** Otherwise straight add the first item of temporary array to the delivery array
+    		 */
+    		deliveryMailItems.add(tempMailItems.getFirst());
+    	}
+    	
+    	boolean isLowerDestFloor = deliveryMailItems.getFirst().getDestFloor() <= Building.MAILROOM_LOCATION;
+    	int size = deliveryMailItems.getFirst().getSize();
+    	for (MailItem item: allMailItems) {
+    		if (item != deliveryMailItems.getFirst()) {
+    			if (size + item.getSize() <= tube.MAXIMUM_CAPACITY) {
+	    			if (item.getDestFloor() <= Building.MAILROOM_LOCATION && isLowerDestFloor) {
+	    				deliveryMailItems.add(item);
+		    			size += item.getSize();
+	    			}
+	    			if (item.getDestFloor() > Building.MAILROOM_LOCATION && !isLowerDestFloor) {
+	    				if (Math.abs(item.getDestFloor() - Building.MAILROOM_LOCATION) < Building.MAILROOM_LOCATION && isShortDistance) {
+	    					deliveryMailItems.add(item);
+		    				size += item.getSize();
+	    				}
+	    				if (Math.abs(item.getDestFloor() - Building.MAILROOM_LOCATION) >= Building.MAILROOM_LOCATION && !isShortDistance) {
+	    					deliveryMailItems.add(item);
+		    				size += item.getSize();
+	    				}
+	    			}
+    			}
+    		}
+    	}
+    	
+    	Comparator<MailItem> sortByDestFloor = (MailItem o1, MailItem o2) -> o2.getDestFloor() - o1.getDestFloor();
+    	deliveryMailItems.sort(sortByDestFloor);
+    }
+	
+	/** Comparator class to compare priority level of mail items 
+	 */
+	private class MailItemComparator implements Comparator<MailItem> {
+		
+		private HashMap<String, Double> priority_level_hashmap;
+		
+		public MailItemComparator() {
+			priority_level_hashmap = new HashMap<String, Double>();
+			priority_level_hashmap.put("LOW", 1.0);
+			priority_level_hashmap.put("MEDIUM", 1.5);
+			priority_level_hashmap.put("HIGH", 2.0);
+		}
+
+		@Override
+		public int compare(MailItem o1, MailItem o2) {
+			double o1_score = priority_level_hashmap.get(o1.getPriorityLevel());
+			double o2_score = priority_level_hashmap.get(o2.getPriorityLevel());
+			
+			if (o1_score > o2_score) {
+				return -1;
+			} else if (o1_score < o2_score) {
+				return 1;
+			}
+			return 0;
+		}
 	}
 
 }
